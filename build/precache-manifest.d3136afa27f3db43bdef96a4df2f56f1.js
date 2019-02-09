467e38da169409cd1128a49ce0fa2164a3e79968
Anton Höglund <antonjhoglund@hotmail.com>
2019-02-09 15:36:12 +0200

    Create initial build

---
+++ build/precache-manifest.d3136afa27f3db43bdef96a4df2f56f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ea00c41ae6d2dc88be0",
+    "url": "/static/js/main.1ea00c41.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "1ea00c41ae6d2dc88be0",
+    "url": "/static/css/main.ec0d9a8a.chunk.css"
+  },
+  {
+    "revision": "b0e20cd15f7265503d7d6a603c1401d4",
+    "url": "/index.html"
+  }
+];
